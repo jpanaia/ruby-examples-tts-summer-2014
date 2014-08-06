48524edb30d109a7d9e7a1250ed4e4bfaa460ebd
Jaime <jpanaia@gmail.com>
2014-08-06 18:53:46 -0400

    update with carrot count...next up: using a loop

--- products.rb
+++ products.rb
@@ -6,9 +6,19 @@ puts 'Enter the new number of bananas'
 # Get response from user
 banana_number = gets.chomp
 
+#Prompt the user to specify carrot count
+puts 'Enter the new number of carrots'
+
+# Get response from user
+carrot_number = gets.chomp
+
 # Update the banana count
 # Redefine the value for 'banana'
 products['banana'] = banana_number
+
+# Update the carrot count
+# Redefine the value for 'carrot'
+products['carrot'] = carrot_number
 
 # Print out the overall inventory
 products.each do |fruit, number|
