2409c039e2507e71dd29c3117494d6beead07042
Jaime <jpanaia@gmail.com>
2014-08-06 19:07:55 -0400

    adding a new key to the hash

--- products.rb
+++ products.rb
@@ -9,7 +9,24 @@ products.each do |fruit, count|
   products[fruit] = gets.chomp
 end
 
+# ask the user for the new fruit name 
+puts "Enter the new fruit name"
+
+# add the new fruit to our hash
+new_fruit = gets.chomp
+
+# add the new fruit to our hash
+products[new_fruit] = 0
+
+# get the count of our new fruit from the user
+puts "How many #{new_fruit}s do you have?"
+new_count = gets.chomp
+
+# set the count of the new fruit
+products[new_fruit] = new_count
+
 # Print out the overall inventory
 products.each do |fruit, number|
   puts "#{fruit}: #{number}"
 end
+
