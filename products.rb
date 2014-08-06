0d2dfde4268a033a01a4bca6e4f8618ea0aafae4
Jaime <jpanaia@gmail.com>
2014-08-06 19:00:33 -0400

    looping through the hash

--- products.rb
+++ products.rb
@@ -1,24 +1,13 @@
 products = { 'banana' => 4, 'carrot' => 10 }
 
-# Prompt the user to specify banana count
-puts 'Enter the new number of bananas'
-
-# Get response from user
-banana_number = gets.chomp
-
-#Prompt the user to specify carrot count
-puts 'Enter the new number of carrots'
-
-# Get response from user
-carrot_number = gets.chomp
-
-# Update the banana count
-# Redefine the value for 'banana'
-products['banana'] = banana_number
-
-# Update the carrot count
-# Redefine the value for 'carrot'
-products['carrot'] = carrot_number
+# loop over each of the products
+products.each do |fruit, count|
+  # Prompt the user to specify the product count
+  puts "Enter the new number of #{fruit}"
+  
+  # Update the count
+  products[fruit] = gets.chomp
+end
 
 # Print out the overall inventory
 products.each do |fruit, number|
